32b43a3a298a2acf680cd3aff3133c128e656fdf
Dan Kendall <dan@thesitedoctor.co.uk>
2012-10-01 17:47:36 +0100

    Added sample and (non-working) code to read it

--- src/Har2Csv/Har2Csv/Program.cs
+++ src/Har2Csv/Har2Csv/Program.cs
@@ -1,9 +1,27 @@
 ﻿namespace Har2Csv
 {
+    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var sampleJson = new StreamReader(new FileStream(@"samples\www.xkcd.com.har", FileMode.Open, FileAccess.Read, FileShare.Read)).ReadToEnd();
+            //var webData = JsonConvert.DeserializeObject<dynamic>(sampleJson);
+            var webData = JObject.Parse(sampleJson);
+            DumpWebData(webData);
+        }
+
+        //private static void DumpWebData(dynamic webData)
+        private static void DumpWebData(JObject webData)
+        {
+            foreach (var entry in webData["entries"])
+            {
+                Console.WriteLine("{0}", entry["request"]["method"] );
+            }
         }
     }
 }
